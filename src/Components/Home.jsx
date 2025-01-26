26d0e0d0921ba42d0941f19db62986de13d6e196
SHETTYKSIDDHARTH <139150666+SHETTYKSIDDHARTH@users.noreply.github.com>
2025-01-26 19:44:33 +0530

    all clear

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ import Contact from './Contact';
 
 const Home = () => {
   return (
-    <div className="mt-25 w-full max-w-screen-lg mx-auto h-fit text-black border-2 overflow-x-hidden px-4 py-2 sm:px-4 bg-white rounded-sm mb-2.5">
+    <div className="mt-25 w-full max-w-screen-lg mx-auto h-fit text-black border-2 overflow-x-hidden px-4 py-2 sm:px-4 bg-white rounded-sm mb-2.5"> 
       <div id="home">
         <Hero />
       </div>
