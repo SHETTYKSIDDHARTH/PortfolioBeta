15ae78b1ba71fccd3e0d5eea0ea6edfcd1b257f1
SHETTYKSIDDHARTH <139150666+SHETTYKSIDDHARTH@users.noreply.github.com>
2025-01-27 22:23:10 +0530

    Final Changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-white text-black overflow-x-hidden">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-white text-black overflow-x-hidden border-b">
       <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative py-4 flex items-center justify-between">
           {/* Logo */}
